e9a2db8a4e122cc317a5efb10fd9393767caaeb7
MuriMan <shahanaabrar2008@gmail.com>
2024-07-15 22:15:33 +0600

    init

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+InitWindow(800, 450, "HI");
+
+while (!WindowShouldClose())
+{
+	BeginDrawing();
+		ClearBackground(Color.Black);
+	EndDrawing();
+}
+
+CloseWindow();
